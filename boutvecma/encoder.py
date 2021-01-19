ec0f63c22aa8851caeea175548cfe3da5ccf817f
Ben Dudson <bd512@york.ac.uk>
2021-01-19 12:35:13 +0000

    Basic working encoder for BOUT++ inp files
    Uses BoutOptions / BoutOptionsFile to read and write
    input files for BOUT++.
    
    Includes basic tests and docstrings.

--- boutvecma/encoder.py
+++ boutvecma/encoder.py
@@ -1,7 +1,47 @@
+import os
+from copy import deepcopy
 from easyvvuq.encoders import BaseEncoder
+from boutdata.data import BoutOptions, BoutOptionsFile
+
 
 class BOUTEncoder(BaseEncoder, encoder_name="bout++_encoder"):
+    def __init__(self, template_input=None):
+        """Read an existing BOUT.inp file to use as a template.
+        If no input is given, an empty set of options will be created
 
-    def encode(self, params={}, target_dir=''):
-        pass
+        Example
+        -------
 
+        from boutvecma.encoder import BOUTEncoder
+        encoder = BOUTEncoder("data/BOUT.inp")
+        """
+        if template_input:
+            self._options = BoutOptionsFile(template_input)
+        else:
+            self._options = BoutOptions()
+
+    def encode(self, params={}, target_dir=""):
+        """Create a BOUT.inp file in target_dir with modified parameters.
+
+        Sub-sections are specified using colons in the params keys
+
+        Example
+        -------
+
+        encoder.encode({"section:key":42}, target_dir="data")
+
+        modifies key so that the file "data/BOUT.inp" contains
+
+        [section]
+        key = 42
+
+        """
+        options = deepcopy(self._options)
+
+        for key, value in params.items():
+            options[key] = value
+
+        # Note: Here options could be BoutOptions or BoutOptionsFile
+        # so can't just use options.write
+        with open(os.path.join(target_dir, "BOUT.inp"), "w") as f:
+            f.write(str(options))
--- boutvecma/test_encoder.py
+++ boutvecma/test_encoder.py
@@ -1,7 +1,55 @@
 from . import encoder
+
+import os
 from easyvvuq.encoders import BaseEncoder
+from boutdata.data import BoutOptionsFile
+
 
 def test_create():
     bout_encoder = encoder.BOUTEncoder()
 
     assert isinstance(bout_encoder, BaseEncoder)
+
+
+def test_encode_from_empty(tmpdir):
+    bout_encoder = encoder.BOUTEncoder()
+
+    newfile = tmpdir.join("BOUT.inp")
+    bout_encoder.encode(
+        params={"nx": 10, "conduction:chi": 12}, target_dir=tmpdir.strpath
+    )
+
+    # Check that the file exists
+    assert os.path.exists(newfile.strpath)
+
+    # Read and check contents
+    options = BoutOptionsFile(newfile.strpath)
+    assert options["nx"] == 10
+    assert options["conduction"]["chi"] == 12
+
+
+def test_encode_identity(tmpdir):
+    # Create a template file
+    template = tmpdir.join("template.inp")
+
+    with open(template.strpath, "w") as f:
+        f.write(
+            """
+        n = 10
+        [section]
+        key = 42
+        """
+        )
+
+    bout_encoder = encoder.BOUTEncoder(template.strpath)
+
+    newfile = tmpdir.join("BOUT.inp")
+    bout_encoder.encode(target_dir=tmpdir.strpath)
+
+    # Check that the file exists
+    assert os.path.exists(newfile.strpath)
+
+    # Read and check contents
+    options = BoutOptionsFile(newfile.strpath)
+    assert options["n"] == 10
+    assert options["section"]["key"] == 42
