6890d0f7cdc4e477aad3da9de60785bdab74747b
Peter Hill <zed.three@gmail.com>
2021-04-23 10:38:26 +0100

    Workaround SC plotting not working

--- example.py
+++ example.py
@@ -45,17 +45,27 @@ distribution_plot_filename = os.path.join(
     f"{campaign.campaign_dir}", "distribution.png"
 )
 
-plt.figure()
-results.plot_moments("T", xlabel=r"$\rho$", filename=moment_plot_filename)
+campaign.save_state(f"{campaign.campaign_dir}/state.json")
+
+fig, ax = plt.subplots()
+xvalues = np.arange(len(results.describe("T", 'mean')))
+ax.fill_between(xvalues, results.describe("T", 'mean') -
+                results.describe("T", 'std'), results.describe("T", 'mean') +
+                results.describe("T", 'std'), label='std', alpha=0.2)
+ax.plot(xvalues, results.describe("T", 'mean'), label='mean')
+try:
+    ax.plot(xvalues, results.describe("T", '1%'), '--', label='1%', color='black')
+    ax.plot(xvalues, results.describe("T", '99%'), '--', label='99%', color='black')
+except RuntimeError:
+    pass
+ax.grid(True)
+ax.set_ylabel("T")
+ax.set_xlabel(r"$\rho$")
+ax.legend()
+fig.savefig(moment_plot_filename)
+
 plt.figure()
 results.plot_sobols_first("T", xlabel=r"$\rho$", filename=sobols_plot_filename)
-
-# d = campaign.get_collation_result()
-# fig, ax = plt.subplots()
-# ax.hist(d.T[0], density=True, bins=50)
-# t1 = results.raw_data["output_distributions"]["T"][49]
-# ax.plot(np.linspace(t1.lower, t1.upper), t1.pdf(np.linspace(t1.lower, t1.upper)))
-# fig.savefig(distribution_plot_filename)
 
 print(
     f"Results are in:\n\t{moment_plot_filename}\n\t{sobols_plot_filename}"
